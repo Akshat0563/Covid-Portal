3b99502e21fc3c771ef3d567044aa6dfc9b6d75f
Jay Patel <pjay20301@gmail.com>
2021-06-27 21:51:53 +0530

    server set up

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//EXPRESS SERVER
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express();
+
+const connectDB = require('./database/connection')
+
+dotenv.config({path: 'config.env'})
+const port = process.env.port
+
+connectDB();
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.get('/', (req,res) => {
+    res.send('Server running')
+})
+
+app.use('/', require('./routes/router'))
+
+app.listen(port, () => {
+    console.log(`server is running on port https://localhost:${port}`)
+});
