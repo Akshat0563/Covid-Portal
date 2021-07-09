f1bb0bbe1d23598b767adeab5625a376094e364a
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2021-07-09 12:45:32 +0530

    Hospitals' basic styling added

--- client/src/components/Hospital/hospital.jsx
+++ client/src/components/Hospital/hospital.jsx
@@ -32,6 +32,7 @@ import React from "react";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+import './hospitals.css'
 
 const api = axios.create({
   baseURL: "http://localhost:2000/api/hospitals",
@@ -58,14 +59,21 @@ const Hospital = () => {
         return "Loading...";
     }
       return (
-    <div>
+    <div class='hospitalMain'>
     {hospitals['data'].map((hospital  => 
         // hospital['hospital']
-        <div>
-            <h1>{hospital['hospital']}</h1>
-            <p>{hospital['address']}</p>
-            <span>{hospital['district']}</span>
-            <span>{hospital['pincode']}</span>
+        <div className='hospitalCard'>
+            <h1 className='hospitalHeader'>{hospital['hospital']}</h1>
+            <div className='card__body'>
+                <p className='hospitalPara'>{hospital['address']}</p>
+                <span className='hospitalSpan'>{hospital['district']}</span>
+                <span>{hospital['pincode']}</span>
+                <div>
+                    Beds Total - <span className='beds bedstotal'>{hospital['beds_total']}</span>
+                    Beds Occupied - <span className='beds bedsoccupied'>{hospital['beds_occupied']}</span>
+                    Beds Available - <span className='beds bedsavailable'>{hospital['beds_available']}</span>
+                </div>
+            </div>
         </div> 
       ))}
     </div>
